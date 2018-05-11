01315f717339a044295c07798fdee0f3dd79bf5f
Xiao Fan <xiao.h.fan@gmail.com>
2018-05-10 22:11:27 -0700

    suspect this should also be quoted (not that functions can contain spaces anyways)

--- intentional-chpwd.zsh
+++ intentional-chpwd.zsh
@@ -5,7 +5,7 @@ _raw_chpwd_functions=("$chpwd_functions[@]")
 _intentional_chpwd() {
   if [ "$ZSH_SUBSHELL" = 0 ]; then
     local FN
-    for FN in $_raw_chpwd_functions[@]; do
+    for FN in "$_raw_chpwd_functions[@]"; do
       "$FN"
     done
   fi
