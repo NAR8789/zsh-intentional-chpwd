292d98f4411a67a70f0960d30b9afe7070a4a3d0
Xiao Fan <xiao.h.fan@gmail.com>
2018-05-11 20:24:59 -0700

    debugging option

--- intentional-chpwd.zsh
+++ intentional-chpwd.zsh
@@ -48,6 +48,16 @@ IntentionalChpwd__onLoad() {
       IntentionalChpwd__replaceExistingChpwd
       ;;
   esac
+
+  if [ "$IntentionalChpwd__option_debug" = 'true' ]; then
+    IntentionalChpwd__debug_onDedupedChange()           { echo 'chpwd_dedupedChange' }
+    IntentionalChpwd__debug_onChange()                  { echo 'chpwd_change' }
+    IntentionalChpwd__debug_onIncludingSubshellChange() { echo 'chpwd_includingSubshellChange'}
+
+    IntentionalChpwd__functions_onDedupedChange+=(IntentionalChpwd__debug_onDedupedChange)
+    IntentionalChpwd__functions_onChange+=(IntentionalChpwd__debug_onChange)
+    IntentionalChpwd__functions_onIncludingSubshellChange+=(IntentionalChpwd__debug_onIncludingSubshellChange)
+  fi
 }
 
 IntentionalChpwd__onLoad
