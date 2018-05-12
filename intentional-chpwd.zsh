166e265f4cdfdc7db6f56a697a2f94e7a4a082bd
Xiao Fan <xiao.h.fan@gmail.com>
2018-05-11 20:19:03 -0700

    option to soft install or manual install

--- intentional-chpwd.zsh
+++ intentional-chpwd.zsh
@@ -1,16 +1,11 @@
 # meant to be sourced from .zshrc
 
-IntentionalChpwd__functions_onDedupedChange=()
-IntentionalChpwd__functions_onChange=()
-IntentionalChpwd__functions_onIncludingSubshellChange=()
+# IntentionalChpwd__functions_onDedupedChange
+# IntentionalChpwd__functions_onChange
+# IntentionalChpwd__functions_onIncludingSubshellChange
 
-IntentionalChpwd__lastWorkingDir="$(pwd)"
-
-IntentionalChpwd__runFunctions() {
-  local fn
-  for fn in "$@"; do
-    "$fn"
-  done
+IntentionalChpwd__init() {
+  IntentionalChpwd__lastWorkingDir="$(pwd)"
 }
 
 IntentionalChpwd__chpwd_run() {
@@ -26,5 +21,33 @@ IntentionalChpwd__chpwd_run() {
   IntentionalChpwd__runFunctions "${IntentionalChpwd__functions_onDedupedChange[@]}"
 }
 
-IntentionalChpwd__functions_onDedupedChange=("${chpwd_functions[@]}")
-chpwd_functions=(IntentionalChpwd__chpwd_run)
+IntentionalChpwd__runFunctions() {
+  local fn
+  for fn in "$@"; do
+    "$fn"
+  done
+}
+
+IntentionalChpwd__replaceExistingChpwd() {
+  IntentionalChpwd__functions_onDedupedChange=("${(@)chpwd_functions:#IntentionalChpwd__chpwd_run}")
+  chpwd_functions=(IntentionalChpwd__chpwd_run)
+}
+
+IntentionalChpwd__softInstall() {
+  chpwd_functions+=(IntentionalChpwd__chpwd_run)
+}
+
+IntentionalChpwd__onLoad() {
+  case "$IntentionalChpwd__option_install" in
+    soft)
+      IntentionalChpwd__softInstall
+      ;;
+    manual)
+      ;;
+    *)
+      IntentionalChpwd__replaceExistingChpwd
+      ;;
+  esac
+}
+
+IntentionalChpwd__onLoad
